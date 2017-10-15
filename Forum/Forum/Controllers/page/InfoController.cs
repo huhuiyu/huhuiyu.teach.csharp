94256d7cd0f60785566dd42aad610dd5383d97e1
huhuiyu <1068306849@qq.com>
2017-10-15 10:11:27 +0800

    添加了帖子详情的跳转

---
+++ Forum/Forum/Controllers/page/InfoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forum.Controllers.page
+{
+    public class InfoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Forum/Forum/Scripts/app/info.js
@@ -0,0 +1,36 @@
+﻿$(function () {
+    var PageInfo = { "PageSize": 2, "PageNumber": 1 };
+
+    function toPage(page) {
+        PageInfo.PageNumber = page;
+        query();
+    }
+
+    $("#btnPre").click(function () {
+        var page = PageInfo.PageNumber - 1;
+        if (page > 0) {
+            toPage(page);
+        }
+    });
+
+    $("#btnNext").click(function () {
+        var page = PageInfo.PageNumber + 1;
+        if (page <= PageInfo.PageCount) {
+            toPage(page);
+        }
+    });
+
+    $("#btnFirst").click(function () {
+        toPage(1);
+    });
+
+    $("#btnLast").click(function () {
+        toPage(PageInfo.PageCount);
+    });
+
+    //获取sid的信息
+    var queryString = location.href.substring(
+        location.href.indexOf("?") + 1);
+    var iid = queryString.substring(4);
+    alert(iid);
+});
